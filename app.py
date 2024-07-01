1aecfe0698dcff4961565402110af6ce2b20ba27
Lohith <69358508+lohithgsk@users.noreply.github.com>
2024-07-02 01:10:15 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import *
+
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.secret_key = "KrrrzPPghtfgSKbtJEQCTA"
+
+
+@app.route('/')
+def dashboard():
+    if request.method == 'GET':
+        return render_template('dashboard.html')
+    else:
+        return render_template('dashboard.html')
